8b17f4a7c1afc9be6e7b3b8de302ca423e05310c
rainbell <rainbellforgit@163.com>
2022-10-20 20:37:02 +0800

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { EventModule } from '@squareboat/nest-events';
+import { UserModule } from './user';
+import { BoatModule } from '@libs/boat';
+import { ConsoleModule } from '@squareboat/nest-console';
+import { ObjectionModule } from '@squareboat/nestjs-objection';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ObjectionModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => config.get('db'),
+      inject: [ConfigService],
+    }),
+    BoatModule,
+    UserModule,
+    EventModule,
+    ConsoleModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
